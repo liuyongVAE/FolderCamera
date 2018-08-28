ddc9d925de1e22989ea05167dea765ec4af64126
LiuYong <1227137391@qq.com>
2018-08-28 15:21:50 +0800

    删除多余代码

--- MyFirstGPUImage/MyFirstGPUImage/Main/CheckComfirm/Controller/CheckViewController.swift
+++ MyFirstGPUImage/MyFirstGPUImage/Main/CheckComfirm/Controller/CheckViewController.swift
@@ -126,12 +126,10 @@ class CheckViewController: UIViewController {
         if image == nil{
             setPreview()
             playView.play()
-            
             //switchFillter(index: 0)
            //self.present(ViewController(), animated: true, completion: nil)
             //  setPreview()
         }
-        detect()
 
     }
     
@@ -172,17 +170,6 @@ class CheckViewController: UIViewController {
             self.view.layoutIfNeeded()
         }
     }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
 
 extension CheckViewController{
@@ -449,54 +436,6 @@ extension CheckViewController:FillterSelectViewDelegate{
         }
     }
     
-    
-    func detect() {
-        guard let personciImage = CIImage(image: image ?? #imageLiteral(resourceName: "斜线")) else {
-            return
-        }
-        let accuracy = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
-        let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: accuracy)
-        let faces = faceDetector?.features(in: personciImage)
-        
-        // Convert Core Image Coordinate to UIView Coordinate
-        let ciImageSize = personciImage.extent.size
-        var transform = CGAffineTransform(scaleX: 1, y: -1)
-        transform = transform.translatedBy(x: 0, y: -ciImageSize.height)
-        
-        for face in faces as! [CIFaceFeature] {
-            
-            print("Found bounds are \(face.bounds)")
-            
-            // Apply the transform to convert the coordinates
-            var faceViewBounds = face.bounds.applying(transform)
-            
-            // Calculate the actual position and size of the rectangle in the image view
-            let viewSize = photoView.bounds.size
-            let scale = min(viewSize.width / ciImageSize.width,
-                            viewSize.height / ciImageSize.height)
-            let offsetX = (viewSize.width - ciImageSize.width * scale) / 2
-            let offsetY = (viewSize.height - ciImageSize.height * scale) / 2
-            
-            faceViewBounds = faceViewBounds.applying(CGAffineTransform(scaleX: scale, y: scale))
-            faceViewBounds.origin.x += offsetX
-            faceViewBounds.origin.y += offsetY
-            
-//            let faceBox = UIView(frame: faceViewBounds)
-//
-//            faceBox.layer.borderWidth = 3
-//            faceBox.layer.borderColor = UIColor.red.cgColor
-//            faceBox.backgroundColor = UIColor.clear
-//            photoView.addSubview(faceBox)
-            
-            if face.hasLeftEyePosition {
-                print("Left eye bounds are \(face.leftEyePosition)")
-            }
-            
-            if face.hasRightEyePosition {
-                print("Right eye bounds are \(face.rightEyePosition)")
-            }
-        }
-    }
     
     
     
